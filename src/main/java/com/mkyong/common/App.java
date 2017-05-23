4799bf9fa785d280d0e9b807267da9b0a5de2892
vsnuvardan <vsnuvardan@gmail.com>
2017-05-23 15:35:38 +0530

    Update App.java
    added sample

--- src/main/java/com/mkyong/common/App.java
+++ src/main/java/com/mkyong/common/App.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Hello world!
+ * Hello world! sample 
  *
  */
 public class App 
